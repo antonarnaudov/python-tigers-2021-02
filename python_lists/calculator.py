5ff808a0ae36aa68fbd75ceb8a6cf733fe29adf8
antonarnaudov <antonarnaudov76@gmail.com>
2021-03-21 12:01:56 +0200

    calculator app finished and new calculator was made using while

--- python_lists/calculator.py
+++ python_lists/calculator.py
@@ -1,9 +1,33 @@
-first_num = int(input('Enter your first number: '))
-second_num = int(input('Enter your second number: '))
-operator = input('Enter your operator: ')
+while True:
+    first_num = int(input('Enter your first number: '))
+    second_num = int(input('Enter your second number: '))
+    operator = input('Enter your operator: ')
 
-print(f'{first_num} {operator} {second_num} = x')
+    if operator == '+':
+        result = first_num + second_num
+        print(f'{first_num} {operator} {second_num} = {result}')
 
-if operator == '+':
-    result = first_num + second_num
-    print(f'{first_num} {operator} {second_num} = {result}')
+    elif operator == '-':
+        result = first_num - second_num
+        print(f'{first_num} {operator} {second_num} = {result}')
+
+    elif operator == '*':
+        result = first_num * second_num
+        print(f'{first_num} {operator} {second_num} = {result}')
+
+    elif operator == '/':
+        result = first_num / second_num
+        print(f'{first_num} {operator} {second_num} = {result}')
+
+    else:
+        print(f'Your operator -> {operator} is invalid, we support only +, -, *, /')
+    print()
+
+    x = input('Do you wish to continue? y / n : ')
+
+    if x == 'n':
+        print()
+        break
+
+
+print('Thanks for playing!')
--- python_lists/demo.py
+++ python_lists/demo.py
@@ -1,13 +1,18 @@
 grade = int(input('My grade is: '))
 
+
 if grade == 2:
     print('Аз съм двойкаджия!')
+
 elif grade == 3:
     print(f'Аз имам оценка {grade},което означава,че имам тройка!')
+
 elif grade == 4:
     print('аз имам четворка')
+
 elif grade == 5:
     print(f'Аз имам оценка {grade}')
+
 elif grade == 6:
     print('аз имам шестица')
 else:
